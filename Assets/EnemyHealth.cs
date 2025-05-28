8f02f94cc706b6eed8559572bd6d792f2ec5cd75
Anastasiia58336 <dsw58336@student.dsw.edu.pl>
2025-05-28 09:54:35 +0200

    Damage Fix

--- Assets/EnemyHealth.cs
+++ Assets/EnemyHealth.cs
@@ -2,14 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyHealth : MonoBehaviour
-{
+public class EnemyHealth : MonoBehaviour { 
+
     public int health = 5;
 
-    public void TakeDamage(int dagame)
-    {
+    public void TakeDamage(int damage) { 
+    
 
-        health += dagame;
+        health -= damage;
         Debug.Log("Enamy took damage. HP: " + health);
 
         if(health <= 0)
