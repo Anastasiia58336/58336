430dede85ef73496974697724cc2eab366c62f4f
Anastasiia58336 <dsw58336@student.dsw.edu.pl>
2025-06-17 18:34:11 +0200

    Enemy Sprite Flip

--- Assets/EnemyFollow.cs
+++ Assets/EnemyFollow.cs
@@ -8,15 +8,26 @@ public class EnemyFollow : MonoBehaviour
     public float speed = 2f;
     private Rigidbody2D rb;
     private Transform player;
+    private SpriteRenderer spriteRenderer;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         player = GameObject.FindGameObjectWithTag("Player").transform;
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
+
     void FixedUpdate()
     {
         if (player == null) return;
+
         Vector2 direction = ((Vector2)player.position - rb.position).normalized;
         rb.MovePosition(rb.position + direction * speed * Time.fixedDeltaTime);
+
+     
+        if (direction.x != 0)
+        {
+            spriteRenderer.flipX = direction.x > 0;
+        }
     }
 }
